0aa690b58c5f29d77ccc7373ce3612c63bc0cda7
JaiRaga <raga.j.santhosh@gmail.com>
2022-12-10 11:02:08 +0530

    changing from numpy to tensor and vice versa

--- basics/tensor.py
+++ basics/tensor.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 # create a tensor
 t = torch.empty(2, 2, 1, 3)
@@ -47,6 +48,53 @@ print("addition", y, y.dtype)
 division = torch.div(z, x)
 div2 = z / x
 print("Division:", division, div2)
+
+# accessing elements
+a = torch.rand(5, 3)
+print("Accessing: Original", a)
+print("Accessing column one:", a[:, 1]) # access the first column
+print("Accessing first row:", a[1, :])
+print("Accessing one element:", a[1, 1], a[1,1].item()) # .item() only works when there is one element in the tensor
+
+# reshaping tensor
+print("Accessing: Original", a)
+b = a.view(15)
+print("reshaped tensor", b, b.size()) # returns 1-d tensor
+b = a.view(-1, 5) # -1 indicates pytorch will figure the right shape
+print("reshap determined by pytorch", b, b.size())
+
+# changing a tensor into numpy array
+print("ones tensor", ones, type(ones))
+ones_np = ones.numpy()
+print("ones numpy", ones_np, type(ones_np))
+
+# Note: if the tensor is on the cpu and not the gpu then
+# both the objects share the same memory location, so if
+# we change one we will also change the other
+
+ones.add_(4)
+print('tensor', ones)
+print('np', ones_np)
+
+# changing numpy array to tensor
+ones_numpy = np.ones(5)
+print("numpy ones:", ones_numpy)
+ones_tensor = torch.from_numpy(ones_numpy)
+print("tensor ones:", ones_tensor)
+ones_numpy += 1
+print("numpy ones:", ones_numpy)
+print("tensor ones:", ones_tensor) # refer the above note
+
+# to use gpu
+if torch.cuda.is_available():
+  device = torch.device('cuda')
+  x = torch.ones(5, device=device)
+  y = torch.ones(5)
+  y = y.to(device) # moves y from cpu to gpu
+  z = x + y
+  z = z.to("cpu") # numpy array can only be stored in cpu
+
+
 
 
 
